7556028c8f0c4847996d5990fa2be3d880566605
Iamneo <techadmin@iamneo.ai>
2023-08-04 05:01:43 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
